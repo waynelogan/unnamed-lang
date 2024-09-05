3719bfa3b6b385d16f0be9569956c4d198f59f35
Wayne Logan <loganamollo@gmail.com>
2024-09-05 22:24:27 +0300

    create types

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
+// following the tutorial: https://youtu.be/-4RmhDy0A2s?list=PLRnI_2_ZWhtA_ZAzEa8uJF8wgGF0HjjEz
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    SEMI,
+    OPEN_PAREN,
+    CLOSE_PAREN,
+} TypeSeparator;
+
+typedef enum {
+    EXIT
+} TypeKeyword;
+
+typedef enum {
+    INT
+} TypeLiteral;
+
+typedef struct {
+    TypeKeyword type;
+} TokenKeyword;
+
+
+typedef struct {
+    TypeSeparator type;
+} TokenSeparator;
+
+typedef struct {
+    TypeLiteral type;
+    int value;
+} TokenLiteral;
+
 int main() {
-    printf("Hello, world\n");
+    TokenLiteral token;
+    token.type = INT;
+    token.value = 5;
+    printf("%d\n", token.value);
 }
