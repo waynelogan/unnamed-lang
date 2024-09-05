e6cf4c6891020e8fe83213a8dc29ade3984180c3
Wayne Logan <loganamollo@gmail.com>
2024-09-05 23:03:10 +0300

    added parsing

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // following the tutorial: https://youtu.be/-4RmhDy0A2s?list=PLRnI_2_ZWhtA_ZAzEa8uJF8wgGF0HjjEz
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef enum {
     SEMI,
@@ -29,15 +30,30 @@ typedef struct {
     int value;
 } TokenLiteral;
 
+ 
+
 void lexer(FILE *file) {
-    char current_char = fgetc(file);
-    while(current_char != EOF) {
-        printf("%c", current_char);
-        current_char = fgetc(file);
+    // read next char from a stream
+    char current = fgetc(file);
+    while(current != EOF) {
+        if (current == ';' ) {
+            printf("found semicolon\n");
+        } else if (current == '(' ) {
+            printf("found open paren\n");
+        } else if (current == ')') {
+            printf("found close paren\n");
+        } else if (isdigit(current)) {
+            // sub ASCII of 0 (is 48) from ascii of number to get number
+            printf("found digit %d\n", current - '0');
+        } else if(isalpha(current)) {
+            printf("found character %c\n", current);
+        }
+        current = fgetc(file);
     }
 }
 
 int main() {
+    // create file stream
     FILE *file;
     file = fopen("test.unn", "r");
     lexer(file);
